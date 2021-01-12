ee3491fcafdf3773d8f314b2c1559b834ebb8971
Jahid1999 <bsse1030@iit.du.ac.bd>
2021-01-13 01:54:14 +0600

    created first page of ui

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,18 +1,56 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    private static final int tile_size = 100;
+    private static final int rows = 6;
+    private static final int columns = 7;
+
+    private Parent createContent() {
+        Pane root = new Pane();
+
+        Shape gridShape = makeGrid();
+        root.getChildren().add(gridShape);
+
+        return  root;
+    }
+
+    private Shape makeGrid() {
+        int width = (columns+1) * tile_size;
+        int height = (rows+1) * tile_size;
+        Shape shape = new Rectangle(columns, rows);
+
+        for(int i=0 ; i<rows; i++) {
+            for(int j=0 ; j<columns; j++) {
+                Circle circle = new Circle(tile_size/2);
+                circle.setCenterX(tile_size/2);
+                circle.setCenterY(tile_size/2);
+                circle.setTranslateX(j* (tile_size + 5) + (tile_size/4));
+                circle.setTranslateY(i* (tile_size + 5) + (tile_size/4));
+
+                shape = Shape.subtract(shape, circle);
+            }
+        }
+
+        shape.setFill(Color.LIGHTBLUE);
+
+        return shape;
+    }
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setScene(new Scene(createContent()));
+        primaryStage.setTitle("Connect Four");
         primaryStage.show();
     }
 
